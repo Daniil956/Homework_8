46e4fec4b3c48a3664a8a0870f06f3227950179c
Даниил <geimer_cok@mail.ru>
2022-12-26 14:40:14 +0300

    G

--- Task_54/Program.cs
+++ Task_54/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.Clear();
+
+int[,] CreateRandomArrey(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange + rightRange);
+        }
+    }
+    return array;
+}
+
+int[,] OrderingArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i,j] < matrix[i+1,j+1])
+            {
+                matrix[i,j] = matrix[i+1,j+1];
+                int tyt = matrix[0,j];
+                matrix[i, 0] = tyt;
+            }
+        }
+    }
+    return matrix;
+}
+
+void ShowArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+
+
+int[,] matrix = CreateRandomArrey(4, 4, 1, 9);
+int[,] arrey = OrderingArray(matrix);
+ShowArray(matrix);
+System.Console.WriteLine();
+ShowArray(arrey);
